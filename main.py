01dfe7ca3852838e18378bf4201ea28fab6db8c6
Jonas <jonas.unglert@gmx.de>
2023-12-11 12:07:12 +0100

    get access token from strava api

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('Hello git')
+import requests
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+auth_url = "https://www.strava.com/oauth/token"
+activites_url = "https://www.strava.com/api/v3/athlete/activities"
+
+payload = {
+    'client_id': "117890",
+    'client_secret': '4ee65c013b3b665ce2e32b1bde6e0023a359b3de',
+    'refresh_token': '579625e6656a485b0ad4f81f01f5af9301ad6c66',
+    'grant_type': "refresh_token",
+    'f': 'json'
+}
+
+print("Requesting Token...\n")
+res = requests.post(auth_url, data=payload, verify=False)
+access_token = res.json()['access_token']
+print("Access Token = {}\n".format(access_token))
